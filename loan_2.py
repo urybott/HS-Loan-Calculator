5355fb2ab5504c29f58e6bb8367f642b31afb476
urybott <95245032+urybott@users.noreply.github.com>
2021-12-16 17:33:36 +0200

    Add files via upload

---
+++ loan_2.py
@@ -0,0 +1,70 @@
+msg_ = [""] * 10
+msg_[0] = "Enter the loan principal:"
+msg_[1] = 'What do you want to calculate?\ntype "m" for number of monthly payments,\ntype "p" for the monthly payment:'
+msg_[2] = 'Enter the monthly payment:'
+msg_[3] = "Enter the number of months:"
+msg_[4] = 'It will take {} month{} to repay the loan'
+msg_[5] = 'Your monthly payment = '
+msg_[6] = ' and the last payment = '
+msg_[7] = 'Invalid input. Please try again.'
+
+def is_str_int(v):
+    res = False
+    if len(v) > 0:
+        if v[0] in "-+" :
+            v = v[1:]
+        if v.isdigit():
+            res = True
+    return res
+
+def int_imput():
+    a = 0
+    m = 7
+    while m:
+        i = input()
+        if not is_str_int(i):
+            print(msg_[m])
+        else:
+            a = int(i)
+            if a > 0:
+                m = 0
+            else:
+                print(msg_[m])
+    return a
+
+def str_imput(choices=""):
+    a = ""
+    m = 7
+    while m:
+        i = input()
+        if not choices or i in choices:
+            a = i
+            m = 0
+        else:
+            print(msg_[m])
+    return a
+
+
+print(msg_[0])
+amount = int_imput()
+print(msg_[1])
+user_choices = str_imput("mp")
+if user_choices == "m":
+    print(msg_[2])
+    payment = int_imput()
+    months = int(amount / payment)
+    if amount % payment:
+        months +=1
+    s = "s" if months != 1 else ""
+    print(msg_[4].format(months,s))
+else:
+    print(msg_[3])
+    months = int_imput()
+    payment = int(amount / months)  # + 0.5
+    lastpayment = 0
+    if amount % months:
+        payment += 1
+        lastpayment = (amount - (months - 1) * payment)
+
+    ret = msg_[5] + str(payment) + (msg_[6] + str(lastpayment) if lastpayment else "")
+    print(ret)
---
+++ loan_3.py
@@ -0,0 +1,115 @@
+import math
+
+
+msg_ = [""] * 15
+msg_[0] = "Enter the loan principal:"
+msg_[1] = '''What do you want to calculate?
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:'''
+msg_[2] = 'Enter the annuity payment:'
+msg_[3] = "Enter the number of periods:"
+msg_[4] = 'It will take {} month{} to repay the loan'
+msg_[5] = 'Your monthly payment = '
+msg_[6] = ' and the last payment = '
+msg_[7] = 'Invalid input. Please try again.'
+msg_[8] = 'Enter the loan interest:'
+msg_[9] = 'Your loan principal = {}!'
+msg_[10] = 'It will take '
+msg_[11] = '{} year{} '
+msg_[12] = 'and '
+msg_[13] = '{} month{} '
+msg_[14] = 'to repay this loan!'
+
+def is_num_str(v):
+    isnum = False
+    if len(v):
+        if v[0] in "-+":
+            v = v[1:]
+        v = v.split(".", 1)
+        if "".join(v).isdigit():
+            isnum = True
+    return isnum
+
+def num_imput():
+    a = 0
+    m = 7
+    while m:
+        i = input()
+        if not is_num_str(i):
+            print(msg_[m])
+        else:
+            a = float(i)
+            if a >= 0:
+                m = 0
+            else:
+                print(msg_[m])
+    return a
+
+def str_imput(choices=""):
+    a = ""
+    m = 7
+    while m:
+        i = input()
+        if not choices or i in choices:
+            a = i
+            m = 0
+        else:
+            print(msg_[m])
+    return a
+
+
+# print(msg_[0])
+# amount = int_imput()
+print(msg_[1])
+user_choices = str_imput("nap")
+
+principal = 0
+payment = 0
+months = 0
+interest = 0
+
+if user_choices != "p":
+    print(msg_[0])
+    principal = num_imput()
+if user_choices != "a":
+    print(msg_[2])
+    payment = num_imput()
+if user_choices != "n":
+    print(msg_[3])
+    months = num_imput()
+print(msg_[8])
+interest = num_imput()
+
+if user_choices == "n":
+    i = interest / 1200
+    all_months = math.ceil(math.log(payment / (payment - i * principal), i + 1))
+    #all_months = int(principal / payment)
+    # if amount % payment:
+        # all_months +=1
+    years = int(all_months / 12)
+    months = all_months % 12
+    sm = "" if months == 1 else "s"
+    sy = "" if years == 1 else "s"
+    res = msg_[10]
+    res += msg_[11].format(years, sy) if years > 0 else ""
+    res += msg_[12] if years > 0 and months > 0 else ""
+    res += msg_[13].format(months, sm) if months > 0 else ""
+    res += msg_[14]
+    print(res)
+elif user_choices == "p":
+    i = interest / 1200
+    principal = int(payment / ((i * (1 + i) ** months) / (((1 + i) ** months - 1))))
+    print(msg_[9].format(principal))
+else:
+    # user_choices == "a"
+    i = interest / 1200
+    payment = math.ceil(principal * ((i * (1 + i) ** months) / (((1 + i) ** months - 1))))
+    lastpayment = 0
+    # payment = int(amount / months)  # + 0.5
+    # if amount % months:
+        # payment += 1
+    # lastpayment = principal % months  #
+    # lastpayment = (principal - (months - 1) * payment)
+    ret = msg_[5] + str(payment) + (msg_[6] + str(lastpayment) if lastpayment else "") + "!"
+    print(ret)
